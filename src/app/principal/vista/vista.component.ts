b952515c2ec2a7d82663c193e7bba71cb8ba693c
Jhon Bellido <jhonbellidovargas@gmail.com>
2023-01-20 15:12:54 -0500

    para reto

--- src/app/principal/vista/vista.component.ts
+++ src/app/principal/vista/vista.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-vista',
   templateUrl: './vista.component.html',
-  styleUrls: ['./vista.component.css']
+  styleUrls: ['./vista.component.css'],
 })
 export class VistaComponent {
-
+  dataPedidos = [
+    {
+      id: 1,
+      nombre: 'Juan Perez',
+      fecha: '2020-01-01',
+      total: 1000,
+      entregado: false,
+    },
+    {
+      id: 2,
+      nombre: 'Maria Lopez',
+      fecha: '2020-01-02',
+      total: 2000,
+      entregado: true,
+    },
+    {
+      id: 3,
+      nombre: 'Pedro Martinez',
+      fecha: '2020-01-03',
+      total: 3000,
+      entregado: true,
+    },
+    {
+      id: 4,
+      nombre: 'Jose Gomez',
+      fecha: '2020-01-04',
+      total: 4000,
+      entregado: false,
+    },
+  ];
 }
